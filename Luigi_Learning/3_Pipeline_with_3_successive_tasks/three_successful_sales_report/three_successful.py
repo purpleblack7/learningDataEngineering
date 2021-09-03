7b0d509c092bc6ea5303db125b39a1ce2ee37159
purpleblack7 <59021184+purpleblack7@users.noreply.github.com>
2021-09-03 07:28:49 -0400

    Add files via upload

---
+++ Luigi_Learning/3_Pipeline_with_3_successive_tasks/three_successful_sales_report/three_successful.py
@@ -0,0 +1,54 @@
+import luigi
+from luigi import Task,LocalTarget
+
+class ProcessOrders(Task):
+    def output(self):
+        return LocalTarget('orders.csv')
+
+    def run(self):
+        with self.output().open('w') as f:
+            print('May,100', file = f)
+            print('May,180', file = f)
+            print('June,200', file = f)
+            print('June,150', file = f)
+
+class GenerateReport(Task):
+    def requires(self):
+        return ProcessOrders()
+
+    def output(self):
+        return LocalTarget('report.csv')
+
+    def run(self):
+        report = {}
+        for line in self.input().open():
+            month,amount = line.split(',')
+            if month in report:
+                report[month] += float(amount)
+            else:
+                report[month] = float(amount)
+
+        with self.output().open('w') as out:
+            for month in report:
+                print(month + ',' + str(report[month]), file = out)
+
+
+class SummarizeReport(Task):
+    def requires(self):
+        return GenerateReport()
+
+    def output(self):
+        return LocalTarget('summary.txt')
+
+    def run(self):
+        total = 0.0
+        for line in self.input().open():
+            month, amount = line.split(',')
+            total += float(amount)
+
+        with self.output().open('w') as f:
+            f.write(str(total))
+
+
+if __name__ == '__main__':
+    luigi.run(['SummarizeReport'])
