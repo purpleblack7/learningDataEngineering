9ecc3d21e73a901698753f884855e1edfae21ad3
purpleblack7 <59021184+purpleblack7@users.noreply.github.com>
2021-09-04 19:58:05 -0400

    Add files via upload

---
+++ Luigi_Learning/7_Dynamic_Tasks/pipeline-with-dynamic-tasks/dynamic-tasks.py
@@ -0,0 +1,40 @@
+import luigi
+import os
+from luigi import Task, LocalTarget, Parameter
+INPUT_FOLDER = 'input'
+OUTPUT_FOLDER = 'output'
+
+class DownloadFile(Task):
+    file_name = Parameter()
+
+    def output(self):
+        path = os.path.join(OUTPUT_FOLDER, self.file_name)
+        return LocalTarget(path)
+
+    def run(self):
+        input_path = os.path.join(INPUT_FOLDER, self.file_name)
+        with open(input_path) as f:
+            with self.output().open('w') as out:
+                for line in f:
+                    if ',' in line:
+                        out.write(line)
+
+class DownloadSalesData(Task):
+    def output(self):
+        return LocalTarget('all_sales.csv')
+
+    def run(self):
+        processed_files = []
+        for file in os.listdir(INPUT_FOLDER):
+            target = yield DownloadFile(file)
+            processed_files.append(target)
+
+        with self.output().open('w') as out:
+            for file in processed_files:
+                with file.open() as f:
+                    for line in f:
+                        out.write(line)
+
+
+if __name__ == '__main__':
+    luigi.run(['DownloadSalesData'])
